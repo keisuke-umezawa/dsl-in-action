64377fcd808b7296b4482b2e9c6a04a6dfd33383
keisuke-umezawa <keisuke.umezawa@gmail.com>
2015-11-25 23:18:16 +0900

    commited chaper2 samples.

---
+++ chapter2/Order.fs
@@ -0,0 +1,47 @@
+﻿module Order 
+type BuyOrSellType =
+    | Buy
+    | Sell
+    override x.ToString() =
+        match x with
+        | Buy -> "Buy"
+        | Sell -> "Sell"
+
+type Order =
+    { Security : string;
+        Quantity : int;
+        LimitPrice : int;
+        BuyOrSell : BuyOrSellType;
+    }
+    static member Default = 
+        { Security = "";
+          Quantity = 0;
+          LimitPrice = 0;
+          BuyOrSell = Buy;
+        }
+
+
+    member x.buy(security, quantity) =
+        { Security = security;
+          Quantity = quantity;
+          LimitPrice = x.LimitPrice;
+          BuyOrSell = Buy;
+        }
+
+    member x.sell(security, quantity) =
+        { Security = security;
+          Quantity = quantity;
+          LimitPrice = x.LimitPrice;
+          BuyOrSell = Sell;
+        }
+
+    member x.limitPrice(price) =
+        { Security = x.Security;
+          Quantity = x.Quantity;
+          LimitPrice = price;
+          BuyOrSell = Sell;
+        }
+
+    member x.valueAs(f) : int =
+        f x.Quantity x.LimitPrice
+
--- chapter2/Tutorial.fsx
+++ chapter2/Tutorial.fsx
@@ -1,3 +1,7 @@
-﻿
+﻿#load "Order.fs"
 
+open Order
 
+let pricer quantity price = quantity * price
+
+let o = Order.Default.buy("MSFT", 10).limitPrice(200).valueAs(pricer)
