abce8de104db4f27c6d9fc60c8cb8b3b287b7708
keisuke-umezawa <keisuke.umezawa@gmail.com>
2015-12-16 23:24:29 +0900

    committed chapter3.

---
+++ chapter3_scalar/Account.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter3scalar {
+    public class Account { 
+        public enum StatusEnum {
+            Open = 0,
+            Closed
+        }
+
+        public enum TypeEnum {
+            Trading = 0,
+            Settlement,
+            Both
+        }
+        
+        public Account(String number, String firstName)
+        {
+            number_ = number;
+            firstName_ = firstName;
+        }
+
+        public Account(String number, String firstName, TypeEnum accountType)
+        {
+            number_ = number;
+            firstName_ = firstName;
+            accountType_ = accountType;
+        }
+
+        public double calculate(Calculator c)
+        {
+            interestAccured_ = c.calculate(this);
+            return interestAccured_;
+        }
+        
+        public bool isOpen()
+        {
+            return status_ == StatusEnum.Open;
+        }
+
+        public Account addName(String name)
+        {
+            names_.Add(name);
+            return this;
+        }
+        /// private member and getter
+        private String number_;
+        public String Number_
+        {
+            get
+            {
+                return number_;
+            }
+        }
+        private String firstName_;
+        public String FirstName_
+        {
+            get
+            {
+                return number_;
+            }
+        }
+        private List<String> names_ = new List<string>();
+        public List<String> Names_
+        {
+            get
+            {
+                return names_;
+            }
+        }
+        private StatusEnum status_ = StatusEnum.Open;
+        public StatusEnum Status_
+        {
+            get
+            {
+                return status_;
+            }
+        }
+        private TypeEnum accountType_ = TypeEnum.Trading;
+        public TypeEnum AccountType
+        {
+            get
+            {
+                return accountType_;
+            }
+        }
+        private double interestAccured_;
+        public double InteresstAccured_
+        {
+            get
+            {
+                return interestAccured_;
+            }
+        }
+    }
+
+    public interface Calculator {
+        double calculate(Account account);
+    }
+    
+    public class CalculatorImpl : Calculator {
+        public double calculate(Account account)
+        {
+            return 1.0;
+        }
+
+    }
+}
---
+++ chapter3_scalar_script/Tutorial.fsx
@@ -0,0 +1,30 @@
+﻿#r @"Q:\umezawa\dev\local\fsharp\dsl-in-action\chapter3_scalar\bin\Debug\chapter3_scalar.dll"
+
+type AccountDsl(account : chapter3scalar.Account) =
+    let _account = account
+    member x.names() = _account.Names_
+
+    member x.belongsTo(name : string) =
+        name.Contains(_account.FirstName_) || _account.Names_.Contains(name)
+      
+    member x.addName(name : string) =
+        AccountDsl(_account.addName(name))
+        
+
+let inline (<<) (account : AccountDsl) (name : string) =
+    account.addName(name)
+    
+
+let account = chapter3scalar.Account("55", "Keisuke")
+
+account.Names_
+
+let accountDsl = AccountDsl(account)
+
+accountDsl.names()
+
+accountDsl << "Keisuke"
+
+accountDsl.belongsTo("Keisuke")
+
+accountDsl.belongsTo("Yusuke")
