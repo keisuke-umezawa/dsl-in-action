1d34e2db089670f03b3191ed2ef598d538af375d
keisuke-umezawa <keisuke.umezawa@gmail.com>
2016-01-13 23:07:05 +0900

    add

---
+++ dependency_injection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dependency_injection
+{
+
+    public
+        class TenuantStore
+    {
+        public TenuantStore()
+        {
+        }
+        public TenuantStore getTenant(string tenant)
+        {
+            return TenuantStore();
+        }
+        public IEnumerable<string> getTenantNames()
+        {
+            return Array<string>();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
