88eca6e0522530e353ba792d9e5c58b0d21a0ba7
keisuke umezawa <keisuke.umezawa@gmail.com>
2016-02-04 21:20:18 +0900

    changed decorator.

---
+++ chapter4/decorator2.fsx
@@ -0,0 +1,33 @@
+﻿open System
+// decorator
+type Trade =
+    { Reference : string;
+      Account : string;
+      Instrument : string;
+      Calculator : unit -> double; }
+    member this.value = this.Calculator()
+    static member make ref account instrument principal =
+        { Reference = ref; 
+          Account = account;
+          Instrument = instrument;
+          Calculator = (fun() -> principal); }
+        
+let defaultCalculator principal = 
+    fun () -> principal
+
+let taxFee(calculateFunctor) =
+    fun () -> calculateFunctor() * 0.8 
+
+let comissionFee(calculateFunctor) =
+    fun () -> calculateFunctor() + 2.0
+
+let taxFeeDecorator (trade : Trade) = 
+    { trade with Calculator = taxFee(trade.Calculator) }
+let comissionDecorator (trade : Trade) = 
+    { trade with Calculator = comissionFee(trade.Calculator) }
+
+let d = Trade.make "a" "b" "c" 100.
+
+let decorated = d |> taxFeeDecorator |> comissionDecorator
+
+decorated.value;;
