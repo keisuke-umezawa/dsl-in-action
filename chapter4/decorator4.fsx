9f233632f4a399d23d09cb4ed73601a14f1fe004
keisuke umezawa <keisuke.umezawa@gmail.com>
2016-02-08 00:25:57 +0900

    added

---
+++ chapter4/decorator4.fsx
@@ -0,0 +1,27 @@
+﻿open System
+// decorator
+type Trade =
+    { Reference : string;
+      Account : string;
+      Instrument : string;
+      Value : double; }
+    static member make reference account instrument principal =
+        { Reference = reference; 
+          Account = account;
+          Instrument = instrument;
+          Value = principal; }
+    member x.evaluate() = x.Value
+    static member (=>) (trade : Trade, decorator) =
+        trade.evaluate |> decorator
+        
+
+let taxFeeDecorator (calculator) = 
+    let taxFee calculateFunctor = fun () -> calculateFunctor() * 0.8 
+    taxFee(calculator)
+
+
+let d = Trade.make "a" "b" "c" 100.
+
+let decoratad = d => taxFeeDecorator
+
+decorated();;
---
+++ chapter4/portfolio.fsx
@@ -0,0 +1,24 @@
+﻿// Listing 4.10 Generic Implementation of groupBy
+
+type Instrument = string
+type Quantity = int
+type TradedQuantity = TradedQuantity of Instrument * Quantity
+
+let instrument (TradedQuantity(i, _)) = i
+let quantity (TradedQuantity(_, q)) = q
+
+type Account = string
+type ActivityReport = ActivityReport of Account * TradedQuantity list
+with member x.groupBy (fn: TradedQuantity -> 'T) =
+            match x with
+            | ActivityReport(name, tqs) ->
+                tqs |> Seq.groupBy fn |> Seq.toList
+
+let tradeList xs = List.map TradedQuantity xs
+
+let activityReport = 
+    ActivityReport("keisuke", 
+        tradeList [("IBM", 360); ("GOOGLE", 200); ("YAHOO", 350); ("TOYOTA", 350);])
+      
+printfn "Activity Report grouping by instrument:\n %A" <| activityReport.groupBy(instrument)
+printfn "Activity Report grouping by quantity:\n %A" <| activityReport.groupBy(quantity)
